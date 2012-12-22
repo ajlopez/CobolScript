d961df066d24e2220d90864b25fd895713915349
ajlopez <webmaster@ajlopez.com>
2012-12-22 19:57:30 -0300

    Run two moves test

--- test/run.js
+++ test/run.js
@@ -44,3 +44,24 @@ var result = null;
 cobs.run(text, null, program);
 
 assert.equal(program.data.working_storage.a, 1);
+
+// compile and run two moves
+
+var program = cobs.compile('move 1 to a. move 2 to b.');
+
+program.data = {
+    working_storage: {
+        a: null,
+        b: null
+    }
+};
+
+var text = program.command.compile(program);
+
+var result = null;
+
+cobs.run(text, null, program);
+
+assert.equal(program.data.working_storage.a, 1);
+assert.equal(program.data.working_storage.b, 2);
+
