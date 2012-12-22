e58b814ad6d6c233c3300dbc28b40ad5e86a77d0
ajlopez <webmaster@ajlopez.com>
2012-12-22 17:42:35 -0300

    multi level group/items in working storage, test

--- test/ws.js
+++ test/ws.js
@@ -52,3 +52,33 @@ assert.ok(program.data.working_storage);
 assert.ok(typeof(program.data.working_storage.group1) != 'undefined');
 assert.ok(typeof(program.data.working_storage.group1.items.item1) != 'undefined');
 assert.ok(typeof(program.data.working_storage.group1.items.item2) != 'undefined');
+
+// Working storage with two group items and three levels
+ 
+var parser = new cobs.Parser('\
+DATA DIVISION.\r\n\
+WORKING-STORAGE SECTION.\r\n\
+01 GROUP1.\r\n\
+02 ITEM1.\r\n\
+03 SUBITEM1.\r\n\
+03 SUBITEM2.\r\n\
+02 ITEM2.\r\n\
+01 GROUP2.\r\n\
+02 ITEM1.\r\n\
+02 ITEM2.\r\n\
+    ');
+
+var program = parser.parseProgram();
+
+assert.ok(program);
+assert.ok(program.data);
+assert.ok(program.data.working_storage);
+assert.ok(program.data.working_storage.group1);
+assert.ok(program.data.working_storage.group1.items.item1);
+assert.ok(program.data.working_storage.group1.items.item1.items);
+assert.ok(typeof(program.data.working_storage.group1.items.item1.items.subitem1) != 'undefined');
+assert.ok(typeof(program.data.working_storage.group1.items.item1.items.subitem2) != 'undefined');
+assert.ok(typeof(program.data.working_storage.group1.items.item2) != 'undefined');
+assert.ok(program.data.working_storage.group2);
+assert.ok(typeof(program.data.working_storage.group2.items.item1) != 'undefined');
+assert.ok(typeof(program.data.working_storage.group2.items.item2) != 'undefined');
