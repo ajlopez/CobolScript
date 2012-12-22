79c6b07a9bde2714013a36ed7ae727b77132bcad
ajlopez <webmaster@ajlopez.com>
2012-12-22 17:38:49 -0300

    Working storage with group item

--- test/ws.js
+++ test/ws.js
@@ -4,7 +4,7 @@ var cobs = require('../'),
 
 // Working storage with one variable
  
- var parser = new cobs.Parser('\
+var parser = new cobs.Parser('\
 DATA DIVISION.\r\n\
 WORKING-STORAGE SECTION.\r\n\
 01 ITEM.\r\n\
@@ -17,3 +17,38 @@ assert.ok(program.data);
 assert.ok(program.data.working_storage);
 assert.ok(typeof(program.data.working_storage.item) != 'undefined');
 
+// Working storage with two variables
+ 
+var parser = new cobs.Parser('\
+DATA DIVISION.\r\n\
+WORKING-STORAGE SECTION.\r\n\
+01 ITEM1.\r\n\
+01 ITEM2.\r\n\
+    ');
+
+var program = parser.parseProgram();
+
+assert.ok(program);
+assert.ok(program.data);
+assert.ok(program.data.working_storage);
+assert.ok(typeof(program.data.working_storage.item1) != 'undefined');
+assert.ok(typeof(program.data.working_storage.item2) != 'undefined');
+
+// Working storage with group item and two subitems
+ 
+var parser = new cobs.Parser('\
+DATA DIVISION.\r\n\
+WORKING-STORAGE SECTION.\r\n\
+01 GROUP1.\r\n\
+02 ITEM1.\r\n\
+02 ITEM2.\r\n\
+    ');
+
+var program = parser.parseProgram();
+
+assert.ok(program);
+assert.ok(program.data);
+assert.ok(program.data.working_storage);
+assert.ok(typeof(program.data.working_storage.group1) != 'undefined');
+assert.ok(typeof(program.data.working_storage.group1.items.item1) != 'undefined');
+assert.ok(typeof(program.data.working_storage.group1.items.item2) != 'undefined');
