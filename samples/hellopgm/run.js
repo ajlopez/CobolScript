12d0ea8b76d53f972a30f2715c6ff71719edd759
ajlopez <webmaster@ajlopez.com>
2012-12-22 11:19:49 -0300

    New hellopgm sample

---
+++ samples/hellopgm/run.js
@@ -0,0 +1,1 @@
+var cobs = require('../..'),    fs = require('fs');function runFile(filename) {    var text = fs.readFileSync(filename).toString();    var parser = new cobs.Parser(text);    var program = parser.parseProgram();    program.procedure();};process.argv.forEach(function(val) {    if (val.slice(-5) == ".cobs" || val.slice(-4) == ".cob")        runFile(val);});
