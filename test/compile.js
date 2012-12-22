d13e69b69b316170023a28777881934dc4d85d84
ajlopez <webmaster@ajlopez.com>
2012-12-22 19:01:23 -0300

    Nested variable in working storage

--- test/compile.js
+++ test/compile.js
@@ -52,3 +52,23 @@ assert.ok(program.data.working_storage);
 var text = program.command.compile(program);
 assert.ok(text);
 assert.ok(text.indexOf('runtime.display(ws.a);') >= 0);
+
+// compile nested variable in program
+
+var program = cobs.compile('\
+data division.\r\n\
+working-storage section.\r\n\
+01 a.\r\n\
+02 b.\r\n\
+procedure division.\r\n\
+display b.\r\n\
+');
+
+assert.ok(program);
+assert.ok(program.command);
+assert.ok(program.data);
+assert.ok(program.data.working_storage);
+
+var text = program.command.compile(program);
+assert.ok(text);
+assert.ok(text.indexOf('runtime.display(ws.a.items.b);') >= 0);
