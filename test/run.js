cad8471e702de73f65dbdb945f3654a4b3ffe0e2
ajlopez <webmaster@ajlopez.com>
2012-12-22 20:19:48 -0300

    Move to two variables run, test

--- test/run.js
+++ test/run.js
@@ -89,3 +89,23 @@ cobs.run(text, null, program);
 assert.equal(program.data.working_storage.group.items.a, 1);
 assert.equal(program.data.working_storage.group.items.b, 2);
 
+// compile and run move to two variables
+
+var program = cobs.compile('move 1 to a, b.');
+
+program.data = {
+    working_storage: {
+        a: null,
+        b: null
+    }
+};
+
+var text = program.command.compile(program);
+
+var result = null;
+
+cobs.run(text, null, program);
+
+assert.equal(program.data.working_storage.a, 1);
+assert.equal(program.data.working_storage.b, 1);
+
