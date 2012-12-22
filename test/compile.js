39c003ef6599a09815fc2ac5ce44e319a6f91c84
ajlopez <webmaster@ajlopez.com>
2012-12-22 18:37:25 -0300

    First compile tests

---
+++ test/compile.js
@@ -0,0 +1,20 @@
+
+var cobs = require('../'),
+    assert = require('assert');
+
+// compile defined
+
+assert.ok(cobs.compile);
+
+// simple compile
+
+var program = cobs.compile('display "hello".');
+
+assert.ok(program);
+assert.ok(program.command);
+
+var text = program.command.compile(program);
+assert.ok(text);
+assert.ok(text.indexOf('runtime.display("hello");') >= 0);
+
+
