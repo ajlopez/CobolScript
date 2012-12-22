48501945d574cd3c7f5fc59378b36a80fef303a5
ajlopez <webmaster@ajlopez.com>
2012-12-22 19:59:00 -0300

    Run two moves to nested items, test

--- test/run.js
+++ test/run.js
@@ -65,3 +65,27 @@ cobs.run(text, null, program);
 assert.equal(program.data.working_storage.a, 1);
 assert.equal(program.data.working_storage.b, 2);
 
+// compile and run two moves to nested items
+
+var program = cobs.compile('move 1 to a. move 2 to b.');
+
+program.data = {
+    working_storage: {
+        group: {
+            items: {
+                a: null,
+                b: null
+            }
+        }
+    }
+};
+
+var text = program.command.compile(program);
+
+var result = null;
+
+cobs.run(text, null, program);
+
+assert.equal(program.data.working_storage.group.items.a, 1);
+assert.equal(program.data.working_storage.group.items.b, 2);
+
