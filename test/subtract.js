3d11db5a8f7d95ad07869c814b90ba9b755315ba
ajlopez <webmaster@ajlopez.com>
2012-12-23 14:52:07 -0300

    new parseProcedures/parseProcedure

--- test/subtract.js
+++ test/subtract.js
@@ -34,3 +34,19 @@ var ws = { a: 1, b: 2 };
 run('subtract 1 from a giving b.', ws);
 assert.equal(ws.a, 1);
 assert.equal(ws.b, 0);
+
+// subtract 1 from variable giving to two variables
+
+var ws = { a: 1, b: 2, c: 3 };
+run('subtract 1 from a giving b c.', ws);
+assert.equal(ws.a, 1);
+assert.equal(ws.b, 0);
+assert.equal(ws.c, 0);
+
+// subtract 1 + 2 from variable giving to two variables
+
+var ws = { a: 1, b: 2, c: 3 };
+run('subtract 1 2 from a giving b c.', ws);
+assert.equal(ws.a, 1);
+assert.equal(ws.b, -2);
+assert.equal(ws.c, -2);
