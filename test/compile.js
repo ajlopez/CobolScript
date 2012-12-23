562d9482225ad982a8ed4738ce2ef4d44485e14e
ajlopez <webmaster@ajlopez.com>
2012-12-23 18:31:01 -0300

    Refactor compile, program + context, applied to perform using

--- test/compile.js
+++ test/compile.js
@@ -118,6 +118,14 @@ assert.ok(text.indexOf('ws.a = 1;') >= 0);
 assert.ok(text.indexOf('ws.b = 2;') >= 0);
 assert.ok(text.indexOf('};') >= 0);
 
+// function with parameters
+
+var text = compile('procedure1 using x, y. move x to a. move y to b.', { a: null, b: null });
+assert.ok(text.indexOf('function procedure1(x, y) {') >= 0);
+assert.ok(text.indexOf('ws.a = x;') >= 0);
+assert.ok(text.indexOf('ws.b = y;') >= 0);
+assert.ok(text.indexOf('};') >= 0);
+
 // perform procedure
 
 var text = compile('perform procedure1.');
