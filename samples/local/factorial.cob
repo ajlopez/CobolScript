3016a5a222e8b9db2801c4f21efbb38099159b3b
ajlopez <webmaster@ajlopez.com>
2012-12-25 08:02:14 -0300

    local sample

---
+++ samples/local/factorial.cob
@@ -0,0 +1,18 @@
+
+* using a local variable instead of an item 01 in working-storage section
+
+local n.
+perform show-factorial using n varying n from 1 to 10.
+
+show-factorial using n local result.
+perform factorial using n giving result.
+display n "! = " result.
+
+factorial using n local m.
+if n = 1 then return n.
+subtract 1 from n giving m.
+perform factorial using m giving m.
+multiply n by m.
+return m.
+
+
