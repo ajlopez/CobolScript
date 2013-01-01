fb4f90c9072bcbffe5f56864f532c114da9e4984
ajlopez <webmaster@ajlopez.com>
2013-01-01 05:34:52 -0300

    Template support in WebRuntime; website sample, WIP, using SimpleWeb and MySQL

---
+++ samples/website/server.js
@@ -0,0 +1,26 @@
+
+var cobs = require('../..');
+    simpleweb = require('simpleweb'),
+    path = require('path'),
+    http = require('http');
+    
+var app = simpleweb();
+
+app.use(simpleweb.query());
+app.use(simpleweb.body());
+app.use(app.router);
+app.use(simpleweb.static(path.join(__dirname, 'public')));
+
+function makeTemplatePage(filename, layout) {
+    var program = cobs.compileTemplateFile(filename);
+    return function (req, res) {
+        program.run(cobs.getRuntime({ request: req, response: res, layout: layout }));
+    };
+}
+
+var layout = cobs.compileTemplateFile('./pages/layout.cobp');
+app.get('/', makeTemplatePage('./pages/index.cobp', layout));
+
+var server = http.createServer(app).listen(8000);
+
+console.log('listening to http://localhost:8000');
