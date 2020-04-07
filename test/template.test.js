ff82a83d817d0ff4be835ee78458d01948cda820
Martin Moxon <moxon6@googlemail.com>
2020-04-07 23:42:59 +0100

    Remove invalid cealrs

--- test/template.test.js
+++ test/template.test.js
@@ -30,44 +30,44 @@ it('compileTemplate defined', () => {
 
 it('compile simple text', () => {
     var text = compile("Hello");
-    expect(!!text.indexOf('runtime.write("Hello");') >= 0).toBe(true);
+    expect(text.indexOf('runtime.write("Hello");') >= 0).toBe(true);
 });
 
 it('simple text with \r \n', () => {
     var text = compile("Hello\r\nWorld");
-    expect(!!text.indexOf('runtime.write("Hello\\r\\nWorld");') >= 0).toBe(true);
+    expect(text.indexOf('runtime.write("Hello\\r\\nWorld");') >= 0).toBe(true);
 });
 
 it('simple text with quotes', () => {
     var text = compile("Hello\"World\"");
-    expect(!!text.indexOf('runtime.write("Hello\\\"World\\\"");') >= 0).toBe(true);
+    expect(text.indexOf('runtime.write("Hello\\\"World\\\"");') >= 0).toBe(true);
 });
 
 it('embedded code', () => {
     var text = compile("<# move 1 to a. #>", { a: null });
-    expect(!!text.indexOf("ws.a = 1;") >= 0).toBe(true);
-    expect(!!(text.indexOf("display") == -1)).toBe(true);
+    expect(text.indexOf("ws.a = 1;") >= 0).toBe(true);
+    expect((text.indexOf("display") == -1)).toBe(true);
 });
 
 it('text and embedded code', () => {
     var text = compile("Hello <# move 1 to a #> world", { a: null });
-    expect(!!text.indexOf("ws.a = 1;") >= 0).toBe(true);
-    expect(!!text.indexOf('runtime.write("Hello ");') >= 0).toBe(true);
-    expect(!!text.indexOf('runtime.write(" world");') >= 0).toBe(true);
+    expect(text.indexOf("ws.a = 1;") >= 0).toBe(true);
+    expect(text.indexOf('runtime.write("Hello ");') >= 0).toBe(true);
+    expect(text.indexOf('runtime.write(" world");') >= 0).toBe(true);
 });
 
 it('text with expression', () => {
     var text = compile("Hello ${a}", { a: null });
-    expect(!!text.indexOf('runtime.write("Hello ", ws.a);') >= 0).toBe(true);
+    expect(text.indexOf('runtime.write("Hello ", ws.a);') >= 0).toBe(true);
 });
 
 it('text with expression and text', () => {
     var text = compile("Hello ${a} World", { a: null });
-    expect(!!text.indexOf('runtime.write("Hello ", ws.a, " World");') >= 0).toBe(true);
+    expect(text.indexOf('runtime.write("Hello ", ws.a, " World");') >= 0).toBe(true);
 });
 
 it('text with expression and text from file', () => {
     var text = compileFile(path.join(__dirname, '/files/hello.cobt'), { a: null });
-    expect(!!text.indexOf('runtime.write("Hello ", ws.a, " World");') >= 0).toBe(true);
+    expect(text.indexOf('runtime.write("Hello ", ws.a, " World");') >= 0).toBe(true);
 });
 
--- test/ws.test.js
+++ test/ws.test.js
@@ -29,8 +29,8 @@ WORKING-STORAGE SECTION.\r\n\
     expect(!!program).toBe(true);
     expect(!!program.data).toBe(true);
     expect(!!program.data.working_storage).toBe(true);
-    expect(!!typeof(program.data.working_storage.item1) != 'undefined').toBe(true);
-    expect(!!typeof(program.data.working_storage.item2) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.item1) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.item2) != 'undefined').toBe(true);
 });
 
 it('Working storage with group item and two subitems', () => {
@@ -47,9 +47,9 @@ WORKING-STORAGE SECTION.\r\n\
     expect(!!program).toBe(true);
     expect(!!program.data).toBe(true);
     expect(!!program.data.working_storage).toBe(true);
-    expect(!!typeof(program.data.working_storage.group1) != 'undefined').toBe(true);
-    expect(!!typeof(program.data.working_storage.group1.item1) != 'undefined').toBe(true);
-    expect(!!typeof(program.data.working_storage.group1.item2) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group1) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group1.item1) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group1.item2) != 'undefined').toBe(true);
 });
 
 it('Working storage with two group items and three levels', () => {
@@ -73,11 +73,11 @@ WORKING-STORAGE SECTION.\r\n\
     expect(!!program.data.working_storage).toBe(true);
     expect(!!program.data.working_storage.group1).toBe(true);
     expect(!!program.data.working_storage.group1.item1).toBe(true);
-    expect(!!typeof(program.data.working_storage.group1.item1.subitem1) != 'undefined').toBe(true);
-    expect(!!typeof(program.data.working_storage.group1.item1.subitem2) != 'undefined').toBe(true);
-    expect(!!typeof(program.data.working_storage.group1.item2) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group1.item1.subitem1) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group1.item1.subitem2) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group1.item2) != 'undefined').toBe(true);
     expect(!!program.data.working_storage.group2).toBe(true);
-    expect(!!typeof(program.data.working_storage.group2.item1) != 'undefined').toBe(true);
-    expect(!!typeof(program.data.working_storage.group2.item2) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group2.item1) != 'undefined').toBe(true);
+    expect(typeof(program.data.working_storage.group2.item2) != 'undefined').toBe(true);
 });
 
